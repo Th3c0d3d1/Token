baf9da231073b2067bbc649313fce6cb038ccd55
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-08-26 16:21:27 -0400

    End wk2 smoke test vid

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,6 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-
 // allows you to log messages to the console
 // for debugging purposes
 import "hardhat/console.sol";
@@ -8,5 +7,5 @@ import "hardhat/console.sol";
 contract Token {
     // name state variable "My Token"
     // public visibility accesses value from outside the contract
-    string public name = "My Token"; 
+    string public name = "Token"; 
 }
