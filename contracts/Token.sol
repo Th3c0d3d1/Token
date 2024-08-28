2fda2281c68f72c494b1ff56b663b8d943999abf
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-08-28 16:25:36 -0400

    end week1 testing

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -5,8 +5,17 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract Token {
-    // name state variable "My Token"
-    // public visibility accesses value from outside the contract
-    string public name = "Nex Gen";
-    string public symbol = "NXG";
+    // "public" visibility accesses value from outside the contract
+    // state vars are stored on the blockchain
+    string public name;
+    string public symbol;
+    uint public decimals = 18;
+    uint public totalSupply;
+
+    constructor(string memory _name, string memory _symbol, uint _totalSupply) {
+        // arg passed into state variable when contract is deployed
+        name = _name;
+        symbol = _symbol;
+        totalSupply = _totalSupply * (10**decimals);
+    }
 }
--- test/Token.js
+++ test/Token.js
@@ -1,16 +1,20 @@
 const { ethers } = require('hardhat')
 const { expect } = require('chai')
 
+const tokens = (n) => {
+    return ethers.utils.parseUnits(n.toString(), 'ether')
+}
+
 describe('Token', () => {
-    // Declare the variable to store in the deployed contract
-    // Make the token variable scope accessible to every function in describe
+    // Declares the variable to store in the deployed contract & makes the token variable scope accessible to every function in describe
     let token
 
     beforeEach(async () => {
         // Fetch the contract factory
         const Token = await ethers.getContractFactory('Token')
         // Assign the value of the deployed contract to the token variable
-        token = await Token.deploy()
+        // State variable token is assigned the value of the deployed contract
+        token = await Token.deploy('Nex Gen', 'NXG', 1000000)
     })
 
     it('has correct name', async () => {
@@ -19,7 +23,17 @@ describe('Token', () => {
     })
 
     it('has correct symbol', async () => {
-        // Read token name & Check the result
+        // Read token symbol & Check the result
         expect(await token.symbol()).to.equal('NXG')
+    })
+
+    it('has correct decimals', async () => {
+        // Read token decimals & Check the result
+        expect(await token.decimals()).to.equal('18')
+    })
+
+    it('has correct total supply', async () => {
+        // Read token total supply & Check the result
+        expect(await token.totalSupply()).to.equal(tokens(1000000))
     })
 })
