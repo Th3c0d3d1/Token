0353c0c49750bdbdff4e9c6d1d23ed33d64f9d4c
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-08-29 15:25:40 -0400

    transfer balance to caller 2

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,12 +1,13 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+
 // allows you to log messages to the console
 // for debugging purposes
 import "hardhat/console.sol";
 
 contract Token {
     // "public" visibility accesses value from outside the contract
-    // state vars are stored on the blockchain
+    // state variables are stored on the blockchain
     string public name;
     string public symbol;
     uint public decimals = 18;
@@ -18,10 +19,22 @@ contract Token {
     // balanceOf is a state variable
     // database stored on the blockchain
     mapping(address => uint256) public balanceOf;
-    // Send tokens from one address to another
+    // Send tokens from one address to another3
 
-    constructor(string memory _name, string memory _symbol, uint _totalSupply) {
-        // arg (local variables) passed into state variable when contract is deployed
+    // Indexed keyword allows you to filter events
+    // Arguments are indexed in the event log
+    event Transfer(
+        address indexed from, 
+        address indexed to, 
+        uint256 value
+        );
+
+    constructor(
+        string memory _name, 
+        string memory _symbol, 
+        uint _totalSupply
+        ){
+        // arg (local variables) passed into state variable @ deployment
         name = _name;
         symbol = _symbol;
         totalSupply = _totalSupply * (10**decimals);
@@ -30,5 +43,23 @@ contract Token {
         // msg is a global variable
         // [msg.sender] read & write to balanceOf mapping
         balanceOf[msg.sender] = totalSupply;
+    }
+
+    function transfer(address _to, uint _value) public returns (bool success) {
+        // Require that the sender has enough tokens
+        // Throw an error if the condition is not met
+        require(balanceOf[msg.sender] >= _value, 'Not enough tokens');
+        require(_to != address(0));
+
+        // Deduct tokens from sender
+        balanceOf[msg.sender] -= _value;
+
+        // Credit the recipient
+        balanceOf[_to] += _value;
+
+        // Emit the event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -7,9 +7,11 @@ const tokens = (n) => {
 
 describe('Token', () => {
     // Declares the variable to store in the deployed contract & makes the token variable scope accessible to every function in describe
+    // Variables stored on blockchain cost more gas than variables stored in memory
     let token, 
         accounts, 
-        deployer
+        deployer,
+        receiver
 
     beforeEach(async () => {
         // Fetch the contract factory
@@ -21,6 +23,7 @@ describe('Token', () => {
         // Get the account(s) from ethers
         accounts = await ethers.getSigners()
         deployer = accounts[0]
+        receiver = accounts[1]
     })
 
     // Declare the variables
@@ -29,7 +32,7 @@ describe('Token', () => {
     const decimals = '18'
     const totalSupply = tokens('1000000')
 
-    // nested describe block
+    // Nested deployment block
     describe('Deployment', () => {
 
         it('has correct name', async () => {
@@ -55,6 +58,55 @@ describe('Token', () => {
         it('assigns the total supply to the deployer', async () => {
             // Read token balance of deployer & Check the result
             expect(await token.balanceOf(deployer.address)).to.equal(totalSupply)
+        })
+    })
+
+    describe('Sending Token', () => {
+        let amount, 
+        transaction, 
+        result
+
+        describe('Success', () => {
+            // Before each test, transfer tokens from deployer to receiver
+            beforeEach(async () => {
+                amount = tokens(100)
+                transaction = await token.connect(deployer).transfer(receiver.address, amount)
+                result = await transaction.wait()
+            })
+
+            it('transfers token balances', async () => {
+                // Transfer tokens from deployer to another account
+                // Connects the token contract to the deployer wallet
+                // neccessary to sign the transaction
+                expect(await token.balanceOf(deployer.address)).to.equal(tokens(999900))
+                expect(await token.balanceOf(receiver.address)).to.equal(amount)
+            })
+
+            it('emits Transfer event', async () => {
+                // Check the events of the transaction
+                const event = result.events[0]
+                expect(event.event).to.equal('Transfer')
+
+                // Check the arguments of the event
+                const args = event.args
+                expect(args.from).to.equal(deployer.address)
+                expect(args.to).to.equal(receiver.address)
+                expect(args.value).to.equal(amount)
+            })
+        })
+
+        describe('Failure', () => {
+            it('rejects insufficient balances', async () => {
+                // Transfer more tokens than the deployer has - 100M
+                const invalidAmount = tokens(100000000)
+                await expect(token.connect(deployer).transfer(receiver.address, invalidAmount)).to.be.reverted
+            })
+
+            it('rejects invalid recipients', async () => {
+                // Transfer tokens to the zero address
+                const amount = tokens(100)
+                await expect(token.connect(deployer).transfer(ethers.constants.AddressZero, amount)).to.be.reverted
+            })
         })
     })
 })
