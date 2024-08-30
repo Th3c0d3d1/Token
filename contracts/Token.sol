2d998df372c047bedc3adf97610a051b4b5e54b1
Th3c0d3d1 <bittradeguy007@gmail.com>
2024-08-30 15:42:14 -0400

    config network & deployed

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: UNLICENSE
 pragma solidity ^0.8.0;
 
 // allows you to log messages to the console
--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,10 +1,15 @@
 async function main() {
     // We get the contract to deploy
     // grabs info from artifacts/contracts/Token.sol/Token.json
-    const Token = await ethers.getContractFactory("My Token");
+    const Token = await ethers.getContractFactory("Token");
+
+    // Define the constructor arguments
+    const name = "Next Gen";
+    const symbol = "NXG";
+    const initialSupply = ethers.utils.parseUnits("1000000", 18); // 1 million tokens with 18 decimals
 
     // Deploy the contract
-    const token = await Token.deploy();
+    const token = await Token.deploy(name, symbol, initialSupply);
     // Gets information about the deployment & load into contract
     await token.deployed();
     console.log(`Token deployed to: ${token.address}`);
